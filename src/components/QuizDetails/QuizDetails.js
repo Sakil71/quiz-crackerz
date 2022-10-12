a4af70d3722d43cb6c55eb2619e3ad994db92295
Shakil Ahamed <sakilahmed7810@gmail.com>
2022-10-12 19:26:29 +0600

    Added Checkbox

--- src/components/QuizDetails/QuizDetails.js
+++ src/components/QuizDetails/QuizDetails.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import './QuizDetails.css'
 
 const QuizDetails = ({ quizes }) => {
-    const { question, options } = quizes;
+    const { question, options, id } = quizes;
 
     return (
-        <div className='question-box border border-red-500 m-4 w-1/2 mx-auto p-5 rounded'>
+        <div className='question-box my-5 border border-blue-200 w-1/2 mx-auto p-5 rounded'>
             <h1 className='text-1xl font-bold text-left'>{question}</h1>
             <div className='questuin-option'>
                 {
-                    options.map(option=> <h1 className='font-semibold text-left ml-10'>{option}</h1>)
+                    options.map(option=> <h1 className='font-semibold text-left ml-10'><input className={id} type="checkbox" /> {option}</h1>)
                 }
             </div>
         </div>
